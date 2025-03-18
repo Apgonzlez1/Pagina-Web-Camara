c3566dd6c8e3c7b03c4164092e72cada2724eb8d
Apgonzlez1 <apgonzlez1@espe.edu.ec>
2025-03-18 12:40:29 -0500

    inicio

--- camaraComercio/src/app/app.routes.ts
+++ camaraComercio/src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { InicioComponent } from './inicio/inicio.component';
 import { NetworkingEventosComponent } from './networking-eventos/networking-eventos.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { BolsaEmpleoComponent } from './bolsa-empleo/bolsa-empleo.component';
+import { AfiliacionComponent } from './afiliacion/afiliacion.component';
+import { DirectorioComponent } from './directorio/directorio.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent }, // Ahora 'Inicio' es la página principal
   { path: 'networking-y-eventos', component: NetworkingEventosComponent },
   { path: 'servicios', component: ServiciosComponent },
   { path: 'bolsa-empleo', component: BolsaEmpleoComponent },
+  { path: 'afiliacion', component: AfiliacionComponent },
+  { path: 'directorio', component: DirectorioComponent },
+  { path: 'contacto', component: ContactoComponent },
 ];

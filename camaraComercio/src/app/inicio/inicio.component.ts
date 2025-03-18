a2e2c48b524b9591b6a2067f91a952d416fdb6fe
Liseth-Poma <lcpoma@espe.edu.ec>
2025-03-17 23:54:25 -0500

    Inicio

--- camaraComercio/src/app/inicio/inicio.component.ts
+++ camaraComercio/src/app/inicio/inicio.component.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
+  imports: [RouterModule, CommonModule],
   standalone: true,
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {}
+export class InicioComponent {
+  backgroundImage: string = 'logo2.png'; // Cambia esto dinámicamente si es necesario
+  
+
+  // Método para cambiar el fondo dinámicamente
+  changeBackground(imageUrl: string) {
+    this.backgroundImage = imageUrl;
+  }
+}

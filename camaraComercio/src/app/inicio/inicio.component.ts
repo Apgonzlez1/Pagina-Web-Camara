ec490b78c3c200e024d91f5d543696d6aa44e13d
Liseth-Poma <lcpoma@espe.edu.ec>
2025-03-20 20:56:17 -0500

    inicio v2

--- camaraComercio/src/app/inicio/inicio.component.ts
+++ camaraComercio/src/app/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -17,6 +17,7 @@ export class InicioComponent {
   changeBackground(imageUrl: string) {
     this.backgroundImage = imageUrl;
   }
+
   quienesSomos = {
     historia: "La Cámara de Industriales y Producción de Santo Domingo se fundó con el objetivo de organizar a los diferentes entes productivos de la región. Desde su creación, ha promovido la unión de los socios y ha representado activamente sus intereses, fomentando el desarrollo y progreso industrial en la región.",
     mision: "Propiciar la productividad y desarrollo regional de la Cámara de Industrias como institución líder de los gremios industriales y de producción, con representación proactiva ante el Estado. Buscamos fortalecer la investigación, el emprendimiento, la competitividad e innovación empresarial para generar empleo y bienestar social.",

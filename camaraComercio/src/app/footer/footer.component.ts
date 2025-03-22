db780eb1e7ef3a8cc933c3cb03953bb674fafa40
Stefanny26 <smhernandez2@espe.edu.ec>
2025-03-21 21:25:57 -0500

    Avance 11.1

--- camaraComercio/src/app/footer/footer.component.ts
+++ camaraComercio/src/app/footer/footer.component.ts
@@ -17,7 +17,9 @@ export class FooterComponent {
     { nombre: 'Facebook', icono: 'fa-brands fa-facebook', url: 'https://www.facebook.com' },
     { nombre: 'Instagram', icono: 'fa-brands fa-instagram', url: 'https://www.instagram.com' },
     { nombre: 'TikTok', icono: 'fa-brands fa-tiktok', url: 'https://www.tiktok.com' },
-    { nombre: 'YouTube', icono: 'fa-brands fa-youtube', url: 'https://www.youtube.com' }
+    { nombre: 'YouTube', icono: 'fa-brands fa-youtube', url: 'https://www.youtube.com' },
+    { nombre: 'Tiktok', icono: 'fa-brands fa-tiktok', url: 'https://www.youtube.com' },
+    { nombre: 'Linkedin', icono: 'fa-brands fa-linkedin', url: 'https://www.linkedin.com' }
   ];
 
   whatsappUrl = 'https://wa.me/1234567890'; // Cambia con el número de WhatsApp de la Cámara

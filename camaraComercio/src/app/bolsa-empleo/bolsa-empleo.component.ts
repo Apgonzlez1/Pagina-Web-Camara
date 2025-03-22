6e030e997fd51aa50a1a71662b4b98c42870baa1
Stefanny26 <smhernandez2@espe.edu.ec>
2025-03-21 19:17:53 -0500

    Avance 11

--- camaraComercio/src/app/bolsa-empleo/bolsa-empleo.component.ts
+++ camaraComercio/src/app/bolsa-empleo/bolsa-empleo.component.ts
@@ -4,12 +4,25 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-bolsa-empleo',
   standalone: true,
-  imports: [CommonModule],  // ⬅ Agregar CommonModule aquí
+  imports: [CommonModule],
   templateUrl: './bolsa-empleo.component.html',
   styleUrls: ['./bolsa-empleo.component.css']
 })
 export class BolsaEmpleoComponent {
-  backgroundImage = 'assets/fondo.jpg';  // ⬅ Definir la imagen de fondo
+  backgroundImage = 'assets/fondo.jpg';
+
+  // Estado del formulario
+  showForm = false;
+
+  // Método para abrir el formulario
+  openForm() {
+    this.showForm = true;
+  }
+
+  // Método para cerrar el formulario
+  closeForm() {
+    this.showForm = false;
+  }
 
   openWhatsApp() {
     window.open('https://api.whatsapp.com/send/?phone=593999989256&text&type=phone_number&app_absent=0', '_blank');

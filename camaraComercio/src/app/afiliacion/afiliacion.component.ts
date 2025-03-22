a8598abe32905e4bf347cc987faf28c5d1d78fe4
Apgonzlez1 <apgonzlez1@espe.edu.ec>
2025-03-22 11:00:11 -0500

    Nuestros Socios

--- camaraComercio/src/app/afiliacion/afiliacion.component.ts
+++ camaraComercio/src/app/afiliacion/afiliacion.component.ts
@@ -15,6 +15,9 @@ export class AfiliacionComponent {
   company: string = '';
   phone: string = '';
   document: File | null = null;
+  sendEmail(email: string) {
+    window.location.href = `mailto:${email}`;
+  }
 
   onFileSelected(event: any) {
     const file = event.target.files[0];
@@ -33,6 +36,85 @@ export class AfiliacionComponent {
     });
 
     // Aquí puedes llamar a un servicio para enviar los datos al backend
+  }
+  searchQuery: string = '';
+
+  companies = [
+    {
+      name: 'Empresa A',
+      contact: 'contacto@empresa-b.com',
+      logo: 'assets/logo1.png',
+      website: 'https://empresa-a.com',
+      sector: 'Manufactura'
+    },
+    {
+      name: 'Empresa B',
+      contact: 'contacto@empresa-b.com',
+      logo: 'assets/logo2.png',
+      website: 'https://empresa-b.com',
+      sector: 'Logística'
+    },
+    {
+      name: 'Empresa C',
+      contact: 'contacto@empresa-b.com',
+      logo: 'assets/logo3.png',
+      website: 'https://empresa-c.com',
+      sector: 'Tecnología'
+    },
+    {
+      name: 'Empresa uno',
+      
+      contact: 'info@empresa-uno.com',
+      logo: 'assets/logo1.png',
+      website: 'https://empresa-a.com',
+      sector: 'Manufactura'
+    },
+    {
+      name: 'Empresa dos',
+      
+      contact: 'contacto@empresa-b.com',
+      logo: 'assets/logo2.png',
+      website: 'https://empresa-b.com',
+      sector: 'Logística'
+    },
+    {
+      name: 'Empresa tres',
+  
+      contact: 'empresa3@tech.com',
+      logo: 'assets/logo3.png',
+      website: 'https://empresa-c.com',
+      sector: 'Tecnología'
+    },
+    {
+      name: 'Empresa A',
+      contact: 'contacto@empresa-b.com',
+      logo: 'assets/logo2.png',
+      website: 'https://empresa-b.com',
+      sector: 'Logística'
+    },
+    {
+      name: 'Empresa BC',
+     
+      contact: 'info@empresabc.com',
+      logo: 'assets/logo3.png',
+      website: 'https://empresa-c.com',
+      sector: 'Tecnología'
+    }
+  ];
+  
+
+  filteredCompanies = [...this.companies];
+
+  filterCompanies() {
+    const query = this.searchQuery.toLowerCase();
+    this.filteredCompanies = this.companies.filter(company =>
+      company.name.toLowerCase().includes(query) ||
+      company.sector.toLowerCase().includes(query)
+    );
+  }
+
+  goToCompanyWebsite(url: string) {
+    window.open(url, '_blank');
   }
 
   openWhatsApp() {

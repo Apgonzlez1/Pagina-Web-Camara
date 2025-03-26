497ef56f0ef057ded8fc041b8acdd9434a389335
Stefanny26 <smhernandez2@espe.edu.ec>
2025-03-26 15:34:48 -0500

    Afiliado

--- camaraComercio/src/app/afiliacion/afiliacion.component.ts
+++ camaraComercio/src/app/afiliacion/afiliacion.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TranslocoModule } from '@jsverse/transloco';
+
 
 @Component({
   selector: 'app-afiliacion',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TranslocoModule],
   templateUrl: './afiliacion.component.html',
   styleUrls: ['./afiliacion.component.css']
 })

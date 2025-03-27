ca80c34df4c906e4b88832732378537addb8c7c5
Stefanny26 <smhernandez2@espe.edu.ec>
2025-03-26 19:17:51 -0500

    Avance 15

--- camaraComercio/src/app/networking-eventos/networking-eventos.component.ts
+++ camaraComercio/src/app/networking-eventos/networking-eventos.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; 
 import { TranslocoModule } from '@jsverse/transloco';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-networking-eventos',
   standalone: true,
-  imports: [CommonModule, RouterModule, TranslocoModule], // ✅ Asegúrate de importar RouterModule
-
+  imports: [CommonModule, RouterModule, TranslocoModule,FormsModule], // ✅ Asegúrate de importar RouterModule
   templateUrl: './networking-eventos.component.html',
   styleUrls: ['./networking-eventos.component.css']
 })
@@ -23,5 +24,18 @@ export class NetworkingEventosComponent {
     
   }  openWhatsApp() {
     window.open('https://api.whatsapp.com/send/?phone=593999989256&text&type=phone_number&app_absent=0', '_blank');
+  }
+  searchText: string = '';
+  eventos = [
+    { nombre: 'Conferencia:Contrato de Inversión, Acuerdos Comerciales y Tratados de Libre Comercio - Capítulo Ecuador 2025', lugar: 'Instituto Tsáchila, Santo Domingo', fecha: 'Jueves 3 de abril del 2025', imagen: 'assets/evento1.jpg' },
+    { nombre: 'Prom Perú', lugar: 'Hotel Golden Vista', fecha: '10 de abril de 2025', imagen: 'assets/evento2.jpg' },
+    { nombre: 'Tsáchila Economic Fórum IV Edition #TEF2025', lugar: '-', fecha: 'Jueves 19 de junio de 2025', imagen: 'assets/Forum1.png' },
+    { nombre: 'Tsáchila Business Convention #TBC2025', lugar: '-', fecha: 'Jueves 23 de octubre 2025', imagen: 'assets/congresos.jpg' }
+  ];
+
+  get filteredEventos() {
+    return this.eventos.filter(evento =>
+      evento.nombre.toLowerCase().includes(this.searchText.toLowerCase())
+    );
   }
 }
